f60da10a325cd0ea3e5b9deaf3bacc45d8d8298a
Esra_Ay <esragedikliay@gmail.com>
2023-07-27 18:15:44 +0200

    fbv

---
+++ 07_FBV/student_api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    home,
+    student_api,
+    student_create,
+    student_detail,
+    student_delete,
+    student_update,
+    )
+
+urlpatterns = [
+    path("", home ),
+    path("student-list",student_api,name="liste"),
+    path("student-create",student_create,name="create"),
+    path("student-detail/<int:pk>",student_detail,name="detail"),
+    path("student-delete/<int:pk>",student_delete,name="delete"),
+    path("student-update/<int:pk>",student_update,name="update")
+   
+]
